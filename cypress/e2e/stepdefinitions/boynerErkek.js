37fb72be84c615aa54b293b288075fbb980ef22d
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-04 21:52:27 +0300

    v

--- cypress/e2e/stepdefinitions/boynerErkek.js
+++ cypress/e2e/stepdefinitions/boynerErkek.js
@@ -10,33 +10,34 @@ Given('Boyner sitesine git', () => {
 
 
 And('arama kutusuna ceket yaz', (searchText)=>{
+    cy.get('#onetrust-accept-btn-handler').click()
     cy.get('.header-top_headerTopSearchInput__hZ7mw').type("ceket")
     cy.get('.search-suggestion_searchBtn__Oemqg > .sc-fqkvVR').click();
 })
 
 When('erkek menusune bas', () =>{
     cy.get(':nth-child(1) > .collapse_root__SrpDo > .collapse_header__LAEcj > .collapse_title__y_n4k').click();
-    cy.get('.checkbox-filter_container__DUb6C > :nth-child(1) > .checkbox_root__86e8X > :nth-child(1) > .checkbox_input__B5l_h').click();
+    cy.get('.checkbox-filter_container__DUb6C > :nth-child(1) > .checkbox_root__86e8X > :nth-child(1) > .checkbox_input__B5l_h').click({force:true});
 
 })
 
 
 And('onerilen siralama tikla',() => {
-    cy.get('.product-list-options_selectedOption__STacx').click();
+    cy.get('.product-list-options_selectedOption__STacx').click({force:true});
     
 })
 
 And('onerilen siralamayi fiyat dusukten yuksege yap',() => {
    
-    cy.get('.product-list-options_options__xWUw0 > :nth-child(2) > :nth-child(1)').click();
+    cy.get('.product-list-options_options__xWUw0 > :nth-child(2) > :nth-child(1)').click({force:true});
 })
 
 Then('fiyatin dusukten yuksege oldugunu dogrula',() => {
     
-    cy.get('[data-key="0"] > .product-item_root__6ZVGl > .product-item_content__9CfBp > a > .product-item_contentStyle__HMrrp > .product-price_root__5EsNB > .product-price_priceContainer__TsWtz > .product-price_checkPrice__NMY9e')
+    cy.get('strong[class')
     .then(($elements) => {
-        const firstProductPrice = parseFloat($elements.eq(0).text().replace('TL', '').replace(',', '.').replace(' ', '')); // İlk ürünün fiyatını alın ve uygun bir veri türüne dönüştürün
-        const secondProductPrice = parseFloat($elements.eq(1).text().replace('TL', '').replace(',', '.').replace(' ', '')); // İkinci ürünün fiyatını alın ve uygun bir veri türüne dönüştürün
+        const firstProductPrice = parseFloat($elements.eq(1).text().replace('TL', '').replace(',', '').replace(' ', '')); // İlk ürünün fiyatını alın ve uygun bir veri türüne dönüştürün
+        const secondProductPrice = parseFloat($elements.eq(2).text().replace('TL', '').replace(',', '').replace(' ', '')); // İkinci ürünün fiyatını alın ve uygun bir veri türüne dönüştürün
     
         // İkinci ürünün fiyatının birinci üründen büyük olduğunu kontrol edin.
         expect(secondProductPrice).to.be.greaterThan(firstProductPrice);
